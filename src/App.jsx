99366b709385a27b282cd0a1ca998ccf6c7ead7d
Jayesh <jayeshjaiswal2510@gmail.com>
2024-08-02 16:32:53 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <>
+      <div className="font-bold text-[45px]">setup completed</div>
       <h1 className=" text-[#c73838] text-[56px]"> web is Working</h1>
     </>
   );
